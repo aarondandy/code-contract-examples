6d031671ed1e9df2c583730b90bed2271804fc0c
Aaron Dandy <aaron.dandy@live.com>
2013-12-30 00:08:53 -0700

    coming up with some examples

--- CC-Samples/Sample/Program.cs
+++ CC-Samples/Sample/Program.cs
@@ -1,17 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Sample
 {
     class Program
     {
         static void Main(string[] args) {
+            var quiet = Transform("Hello.", Char.ToLower);
+            Console.WriteLine("{0} ({1})", quiet, quiet.Length);
+            var yelling = Transform("Hello.", Char.ToUpper);
+            Console.WriteLine("{0} ({1})", yelling, yelling.Length);
 
-
-
+            Console.WriteLine("Transformed: {0}", SuccessfullyTransformedChars);
+            Console.ReadKey();
         }
+
+
+
+
+        public static int SuccessfullyTransformedChars = 0;
+
+        public static string Transform(string source, Func<char, char> tx, bool canYell = false) {
+            if (tx == null) throw new ArgumentNullException("tx"); // legacy require
+            Contract.Requires(source != null);
+            Contract.Ensures(Contract.Result<string>() != null);
+            Contract.EnsuresOnThrow<Exception>(Contract.OldValue(SuccessfullyTransformedChars) == SuccessfullyTransformedChars);
+
+            var result = String.Concat(source.Select(tx));
+            Interlocked.Add(ref SuccessfullyTransformedChars, result.Length);
+
+            if (!canYell && result.Any(Char.IsUpper))
+                throw new InvalidOperationException("NO YELLING!");
+
+            return result;
+        }
+
+
+
     }
 }
--- CC-Samples/SomeLib/Class1.cs
+++ CC-Samples/SomeLib/Class1.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SomeLib
 {
     public class Class1
     {
+
+        
     }
 }
